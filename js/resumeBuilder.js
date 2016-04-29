fb5c84bd317e33b453025af60f6b9aa513d8009a
Eugene208029488 <elgervacio@gmail.com>
2016-04-29 18:13:29 -0400

    Lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//"#main").append("Eugene");
+var awesomethoughts = "I am Eugene and I'm awesome!!!";
+console.log(awesomethoughts);
+var funthoughts=awesomethoughts.replace("awesome","fun");
+console.log(funthoughts);
+//$("#main").append(funthoughts);
+var formattedName = HTMLheaderName.replace("%data%","Eugene Gervacio");
+var formattedRole = HTMLheaderRole.replace("%data%","Architect");
+$("#main").prepend(formattedRole);
+$("#main").prepend(formattedName);
+
